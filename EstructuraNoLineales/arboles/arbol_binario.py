5c41be2d16be4c934257f138201c3ac2f552f492
Antony Str <155707235+Rafaelalon0509@users.noreply.github.com>
2024-03-09 23:19:18 -0600

    ordenar

---
+++ EstructuraNoLineales/arboles/arbol_binario.py
@@ -0,0 +1,27 @@
+class NodoArbol:
+    def __init__(self,valor):
+        self.valor= valor
+        self.izquierda = None
+        self.derecha= None 
+        # crear nodos
+
+class arbol:
+    def preorden(NodoArbol):
+            if (NodoArbol is not None ):
+
+            #print(NodoArbol)
+            #preorden(NodoArbol.izquierda)
+            #preorden(NodoArbol.derecha)
+
+
+nodo1 = NodoArbol(1)
+nodo2 = NodoArbol(2)
+nodo3 = NodoArbol(3)
+
+#conectar nodos 
+nodo1.izquierda = nodo2
+nodo1.derecha = nodo3
+
+
+
+
